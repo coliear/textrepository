2b288e1ac1abe222038be7f2b940f4ca5a965236
lele he <colie141448@gmail.com>
2021-08-02 00:10:09 +0800

    新增

--- test1.py
+++ test1.py
@@ -1,8 +1,11 @@
 from csv import DictReader
-import cvs
-data_rdr = DictReader(open('C:/Users/何长乐/Desktop/11/mn.csv', 'rt', encoding="utf-8"))
-header_rdr = DictReader(open('C:/Users/何长乐/Desktop/11/mn_headers.csv', 'rt', encoding="utf-8"))
+import csv
+data_rdr = DictReader(open('mn.csv', 'rt', encoding="utf-8"))
+header_rdr = DictReader(open('mn_headers.csv', 'rt', encoding="utf-8"))
 data_rows = [d for d in data_rdr]
 header_rows = [h for h in header_rdr]
 print(data_rows[:5])
 print(header_rows[:5])
+for data_dict in data_rows[:5]:
+    for a, b in data_dict.items():
+           print(a, b)
