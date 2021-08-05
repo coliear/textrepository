3c993354ef986d5ce225658bd02a64be75aac669
lele he <colie141448@gmail.com>
2021-08-06 00:17:40 +0800

    8月6日

--- test1.py
+++ test1.py
@@ -1,11 +1,30 @@
-from csv import DictReader
-import csv
-data_rdr = DictReader(open('mn.csv', 'rt', encoding="utf-8"))
-header_rdr = DictReader(open('mn_headers.csv', 'rt', encoding="utf-8"))
+from csv import reader
+
+data_rdr = reader(open('mn.csv', 'rt',encoding="utf-8"))
+header_rdr = reader(open('mn_headers_updated.csv', 'rt',encoding="utf-8"))
 data_rows = [d for d in data_rdr]
-header_rows = [h for h in header_rdr]
-print(data_rows[:5])
-print(header_rows[:5])
-for data_dict in data_rows[:5]:
-    for a, b in data_dict.items():
-           print(a, b)
+header_rows = [h for h in header_rdr if h[0] in data_rows[0]]
+all_short_headers = [h[0] for h in header_rows]
+print('data_rows', data_rows[0])
+print('header_rows:',header_rows)
+skip_index = []
+final_header_rows = []
+# for header in data_rows[0]:
+#     if header not in all_short_headers:
+#         index = data_rows[0].index(header)
+#         skip_index.append(index)
+#     else:
+#         for head in header_rows:
+#             if head[0] == header:
+#                 final_header_rows.append(head)
+#                 break
+# new_data = []
+# for row in data_rows[1:]:
+#     new_row = []
+#     for i, d in enumerate(row):
+#         if i not in skip_index:
+#             new_row.append(d)
+#     new_data.append(new_row)
+# zipped_data = []
+# for drow in new_data:
+#     zipped_data.append(zip(final_header_rows, drow))
---
+++ test2.py
@@ -0,0 +1,4 @@
+list_with_dupes = [1, 5, 6, 2, 5, 6, 8, 3, 8, 3, 3, 7, 9]
+
+set_without_dupes = set(list_with_dupes) 
+print (set_without_dupes)
